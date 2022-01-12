cdb42a6cc233ac3e30850ef997a57ca0d66617e6
Weikun Wu <weikunwu@Weikuns-MacBook-Pro.local>
2022-01-12 01:29:41 -0800

    first deploy to heroku

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from service.HelloApiHandler import HelloApiHandler
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/hello')
